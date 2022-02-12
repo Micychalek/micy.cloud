6d00deaa712441d6701fe42796aace2b789dbd13
Micy <87359225+Micychalek@users.noreply.github.com>
2022-02-12 18:48:59 +0100

    Add files via upload

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,23 +1,23 @@
 let background = document.getElementById('background');
 let images = [
-    './assets/img/wallpaper1.jpg',
-    './assets/img/wallpaper2.jpg',
-    './assets/img/wallpaper3.jpg',
-    './assets/img/wallpaper4.jpg',
-    './assets/img/wallpaper5.jpg',
-    './assets/img/wallpaper6.jpg',
-    './assets/img/wallpaper7.jpg',
-    './assets/img/wallpaper8.jpg',
-    './assets/img/wallpaper9.jpg',
-    './assets/img/wallpaper10.jpg',
-    './assets/img/wallpaper11.jpg',
-    './assets/img/wallpaper12.jpg',
-    './assets/img/wallpaper13.jpg',
-    './assets/img/wallpaper14.jpg',
-    './assets/img/wallpaper15.jpg',
-    './assets/img/wallpaper16.jpg',
-    './assets/img/wallpaper17.jpg',
-    './assets/img/wallpaper18.jpg'
+    './assets/img/wallpaper1.gif',
+    './assets/img/wallpaper2.gif',
+    './assets/img/wallpaper3.gif',
+    './assets/img/wallpaper4.gif',
+    './assets/img/wallpaper5.gif',
+    './assets/img/wallpaper6.gif',
+    './assets/img/wallpaper7.gif',
+    './assets/img/wallpaper8.gif',
+    './assets/img/wallpaper9.gif',
+    './assets/img/wallpaper10.gif',
+    './assets/img/wallpaper11.gif',
+    './assets/img/wallpaper12.gif',
+    './assets/img/wallpaper13.gif',
+    './assets/img/wallpaper14.gif',
+    './assets/img/wallpaper15.gif',
+    './assets/img/wallpaper16.gif',
+    './assets/img/wallpaper17.gif',
+    './assets/img/wallpaper18.gif'
 ]
 let linearGrade = '#000000d9, #000000d9';
 let randInt = Math.floor(Math.random()*images.length)
